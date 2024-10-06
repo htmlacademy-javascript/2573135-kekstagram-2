798b88a229128a16c1166130dc85fa622ea9f8c2
Keks <keks@htmlacademy.ru>
2024-10-06 19:28:25 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,12 @@
+const checkLendth = (string = '', maxSymbols = 1) => (string.length <= maxSymbols);
+
+const isPalindrome = (string) => {
+  string = string.replaceAll (' ', '').toLowerCase(); 
+
+  let reversedLine = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedLine += string[i];
+  }
+
+  return string === reversedLine;
+}
