ed6f0ba0d86463024dda78152ca4ba482e352508
Keks <keks@htmlacademy.ru>
2024-10-24 18:53:39 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,21 @@
+import {getRandomInteger, createComments,numComments} from ".util.js";
+
+const createPhoto = () => {
+  let id = 1;
+
+  return () => {
+    const photo = {};
+    photo.id = id;
+    photo.url = `photo/${id}.jpg`;
+    photo.description = `Фото №${id}`;
+    photo.likes = numLikes();
+    photo.comments = Array.from({length : numComments()}, createComments());
+    id++;
+    return photo;
+
+  };
+};
+
+
+const photoArray = Array.from ({length: CREATED_PHOTO}, createPhoto());
+console.log(photoArray);
